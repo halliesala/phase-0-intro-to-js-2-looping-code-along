e9de6d1a80e8672f2e48d3191e4de9718f3a4ce8
Hallie Sala <halliesala19@gmail.com>
2023-06-07 15:57:35 -0400

    complete looping code-along

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return messages;
+}
+
+function countDown(anInt) {
+    for (let i = anInt; i >= 0; i--) {
+        console.log(i)
+    }
+}
+
